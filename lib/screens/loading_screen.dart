0739a8f75696836a97bab6ee99c77e0e36410e76
Mohammed Habibullah <habibullah786official@gmail.com>
2022-07-01 23:13:43 +0600

    weather api added

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart';
+import 'package:simple_weather/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -9,21 +10,47 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+    getData();
+  }
+
   void getLocation() async {
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
-    print(position);
+    try {
+      Location location = Location();
+      await location.getCurrentLocation();
+      print(Location().longitude);
+      print(Location().latitude);
+    } catch (e, s) {
+      print(s);
+    }
+  }
+
+  final url =
+      'https://samples.openweathermap.org/data/2.5/forecast?q=M%C3%BCnchen,DE&appid=439d4b804bc8187953eb36d2a8c26a02';
+  void getData() async {
+    Response response = await get(Uri.parse(url));
+
+    print(response.body);
   }
 
   @override
   Widget build(BuildContext context) {
+    double doubleMargin;
+    String marginSize = '30';
+    try {
+      doubleMargin = double.parse(marginSize);
+    } catch (e) {
+      print(e);
+      doubleMargin = double.parse(marginSize);
+    }
+
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-            onPressed: () {
-              getLocation();
-            },
-            child: Text('Get the location')),
+      body: Container(
+        color: Colors.red,
+        margin: EdgeInsets.all(doubleMargin),
       ),
     );
   }
